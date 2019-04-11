353f28f150253c6b9f915d06a1c9fcb790ea4366
Nilanjan Biswas 17-35583-3 <nilanjanmotoxforce@gmail.com>
2019-04-12 03:08:16 +0600

    Initial.commit

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        FrameLogin login=new FrameLogin();
+        login.setVisible(true);
         // TODO code application logic here
     }
     
