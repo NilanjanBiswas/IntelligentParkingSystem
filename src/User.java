5bb240a9811fd2c7a747450a8d82c32320a050aa
Nilanjan Biswas 17-35583-3 <nilanjanmotoxforce@gmail.com>
2019-04-11 23:55:34 +0600

    Initial.commit

--- src/User.java
+++ src/User.java
@@ -14,6 +14,48 @@ public class User {
     private String ContactNumber;
     private String VechileId;
     private String UniversityId;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getContactNumber() {
+        return ContactNumber;
+    }
+
+    public void setContactNumber(String ContactNumber) {
+        this.ContactNumber = ContactNumber;
+    }
+
+    public String getVechileId() {
+        return VechileId;
+    }
+
+    public void setVechileId(String VechileId) {
+        this.VechileId = VechileId;
+    }
+
+    public String getUniversityId() {
+        return UniversityId;
+    }
+
+    public void setUniversityId(String UniversityId) {
+        this.UniversityId = UniversityId;
+    }
+
+        
     
     
 }
