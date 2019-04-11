f11d8647228ef046abdb01e9c0f73147d6338647
Nilanjan Biswas 17-35583-3 <nilanjanmotoxforce@gmail.com>
2019-04-11 23:55:51 +0600

    Initial.commit

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -12,6 +12,30 @@ public class Vehicle {
     private int ID;
     private String VehcileType;
     private String VehcileNo;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getVehcileType() {
+        return VehcileType;
+    }
+
+    public void setVehcileType(String VehcileType) {
+        this.VehcileType = VehcileType;
+    }
+
+    public String getVehcileNo() {
+        return VehcileNo;
+    }
+
+    public void setVehcileNo(String VehcileNo) {
+        this.VehcileNo = VehcileNo;
+    }
     
     
 }
